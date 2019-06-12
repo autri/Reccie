30385c8836d1d4e11cd2adb723ce364ea6f4a1e8
Autri Baghkhanian <autri.baghkhanian@gmail.com>
2019-06-11 20:01:24 -0700

    Initial Commit

---
+++ Reccie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reccie
+//
+//  Created by Autri Baghkhanian on 6/11/19.
+//  Copyright © 2019 Autri Baghkhanian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
